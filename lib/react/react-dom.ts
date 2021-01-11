3a046f515d1b950d11f602e3ec8adef4e27fbb47
YeSifan <yesifan66@gmail.com>
2021-01-11 15:29:48 +0800

    独立生成fiber树的方法 剥离调和中生成dom的步骤 将平台无关的调和算法移入reconciler.js

--- lib/react/react-dom.ts
+++ lib/react/react-dom.ts
@@ -1,79 +1,9 @@
 import { React$Elemnt } from '.';
-import { Fiber } from './react-reconciler';
+import { performUnitOfWork, Fiber } from './react-reconciler';
 
 let wipRoot:Fiber;
+let currentRoot:Fiber;
 let nextUnitOfWork:Fiber;
-
-function commitWork(fiber:Fiber){
-  if (!fiber) {
-    return
-  }
-  const domParent = fiber.parent.dom
-  domParent.append(fiber.dom)
-  commitWork(fiber.child)
-  commitWork(fiber.sibling)
-}
-
-function commitRoot(){
-  commitWork(wipRoot.child)
-  wipRoot = null
-}
-
-function wookLoop(){
-  if(nextUnitOfWork){
-    nextUnitOfWork = performUnitOfWork(
-      nextUnitOfWork
-    )
-    window.requestIdleCallback(wookLoop)
-  }
-  if(!nextUnitOfWork && wipRoot){
-    commitRoot()
-  }
-}
-
-
-function performUnitOfWork(fiber:Fiber){
-  // add dom node
-  if (!fiber.dom) fiber.dom = createDom(fiber)
-
-  // 为children生成filber
-  const elements = fiber.props.children
-  let index = 0
-  let prevSibling = null
-  
-  while (index < elements.length) {
-    const element = elements[index]
-  ​
-    const newFiber = {
-      type: element.type,
-      props: element.props,
-      parent: fiber,
-      dom: null,
-    }
-
-    // 第一个子节点添加为父节点的子节点
-    if (index === 0) {
-      fiber.child = newFiber
-    } else {// 之后的子节点添加为上一个节点的兄弟节点
-      prevSibling.sibling = newFiber
-    }
-​
-    prevSibling = newFiber
-    index++
-  }
-
-  // return next unit of work
-  // 搜索下一个运行单元 首先常识 child,
-  // 然后 sibling， 再然后 parent 的 sibling
-  if (fiber.child) return fiber.child
-  let nextFiber = fiber
-  while (nextFiber) {
-    if (nextFiber.sibling) {
-      return nextFiber.sibling
-    }
-    nextFiber = nextFiber.parent
-  }
-}
 
 function createDom(element:React$Elemnt){
   if(element.type === 'TEXT_ELEMENT') return element.props.nodeValue
@@ -88,12 +18,42 @@ function createDom(element:React$Elemnt){
   return node
 }
 
+function commitWork(fiber:Fiber, parent:HTMLElement){
+  if (!fiber) {
+    return
+  }
+  const dom = createDom(fiber)
+  parent.append(dom)
+  commitWork(fiber.child, dom)
+  commitWork(fiber.sibling, dom)
+}
+
+function commitRoot(){
+  commitWork(wipRoot.child, wipRoot.containerInfo)
+  currentRoot = wipRoot
+  wipRoot = null
+}
+
+
+function wookLoop(){
+  if(nextUnitOfWork){
+    nextUnitOfWork = performUnitOfWork(
+      nextUnitOfWork
+    )
+    window.requestIdleCallback(wookLoop)
+  }
+  if(!nextUnitOfWork && wipRoot){
+    commitRoot()
+  }
+}
+
 export function render(element:React$Elemnt, container: HTMLElement){
   wipRoot = {
-    dom: container,
+    containerInfo: container,
     props: {
       children: [element]
     },
+    alternate: currentRoot //link old fiber
   }
   nextUnitOfWork = wipRoot
   wookLoop()
--- lib/react/react-reconciler.ts
+++ lib/react/react-reconciler.ts
@@ -1,9 +1,60 @@
 import { React$Elemnt } from '.'
 
 export interface Fiber extends React$Elemnt{
-  dom?: HTMLElement
+  containerInfo?: HTMLElement
+
+  alternate?: Fiber //未来与现在的双向绑定
 
   parent?: Fiber
   child?: Fiber
   sibling?: Fiber
+}
+
+let workInProgress:Fiber
+
+/**
+ * 在wipFiber上生成新的fiber树
+ */
+function reconcileChildren(wipFiber:Fiber, elements:React$Elemnt[]){
+  let index = 0
+  let oldFiber = wipFiber.alternate?.child
+  let prevSibling = null
+​
+  while (index < elements.length||oldFiber) {
+    const element = elements[index]
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: wipFiber,
+      dom: null,
+    }
+​
+    if (index === 0) {
+      wipFiber.child = newFiber
+    } else {
+      prevSibling.sibling = newFiber
+    }
+​
+    prevSibling = newFiber
+    index++
+  }
+}
+
+
+export function performUnitOfWork(fiber:Fiber){
+  // 为children生成filber
+  const elements = fiber.props.children
+  reconcileChildren(fiber, elements)
+
+  // return next unit of work
+  // 搜索下一个运行单元 首先常识 child,
+  // 然后 sibling， 再然后 parent 的 sibling
+  if (fiber.child) return fiber.child
+  let nextFiber = fiber
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling
+    }
+    nextFiber = nextFiber.parent
+  }
 }
